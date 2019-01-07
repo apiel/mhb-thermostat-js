4c6029c56fb06efac842952c99a6709d55550759
Alexandre Piel <alexandre.piel@gmail.com>
2019-01-07 17:37:20 +0100

    send schedules

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const {
 const warmTemp = 22.5;
 const awayTemp = 15.0;
 
+const config = {
+    start: { temp: 22.5, start_hour: 6, start_minute: 30 },
+    end: { temp: 15.0, start_hour: 23, start_minute: 0 },
+};
+
 const macAddr = ['30:07:4D:ED:EF:86', '8C:8E:F2:B3:9F:47']
 
 function someOneIsAtHome() {
@@ -18,9 +23,9 @@ function someOneIsAtHome() {
     return intersection.length > 0;
 }
 
+const baseCmd = 'cd ../broadlink-thermostat-cli/ && ./broadlink-thermostat-cli.py';
 function getThermostatData() {
-    const cmd = 'cd ../broadlink-thermostat-cli/ && ./broadlink-thermostat-cli.py';
-    const result = execSync(cmd, { encoding: 'utf8' });
+    const result = execSync(baseCmd, { encoding: 'utf8' });
     const results = result.split("\n")
 
     if (results.length === 12) {
@@ -30,27 +35,52 @@ function getThermostatData() {
     }
 }
 
+function setThermostatSchedules(schedules) {
+    const cmd = `${baseCmd} --schedule='${JSON.stringify(schedules)}'`;
+    const result = execSync(cmd, { encoding: 'utf8' });
+    console.log('setThermostatSchedules', result);
+}
+
 function start() {
-    const { hour, min, thermostat_temp } = getThermostatData();
+    const data = getThermostatData();
+    if (!data) {
+        console.log('Cannot access thermostat to get data');
+        return;
+    }
+    const { hour, min, thermostat_temp } = data;
 
     const currentTime = moment({ hour, minute: min });
-    const startTime = moment('06:30', 'HH:mm');
-    const endTime = moment('23:00', 'HH:mm');
+    const startTime = moment({ hour: config.start.start_hour, minute: config.start.start_minute });
+    const endTime = moment({ hour: config.end.start_hour, minute: config.end.start_minute });
 
     isHeatingTime = currentTime.isBetween(startTime , endTime);
-    let temperature = awayTemp;
+    let temp = config.end.temp;
     if (isHeatingTime && someOneIsAtHome()) {
-        temperature = warmTemp;
+        temp = config.start.temp;
     }
-    console.log(`need to have temp ${temperature}`);
+    console.log(`need to have temp ${temp}`);
 
-    if (thermostat_temp !== temperature) {
-        console.log('need to update temperature of the thermostat.');
-    } else {
+    if (thermostat_temp === temp) {
         console.log('thermostat is already set to this temperature. No need to do anything.');
+    } else {
+        console.log('need to update temperature of the thermostat.');
+
+        const schedules = [[
+            config.start,
+            config.start,
+            { temp, start_hour: hour, start_minute: min },
+            config.end,
+            config.end,
+            config.end,
+        ], [ config.start, config.end ]];
+
+        console.log('new schedules', schedules);
+        setThermostatSchedules(schedules);
     }
 }
 start();
 
-// we could do this, only if manuel mode is activated
-// we could check if temp is different than 22 or 15 before to change it
+// if thermostat_temp !== than awayTemp && warmTemp dont do anything
+// but if nobody is home for more than 1h then set awayTemp
+
+// --schedule='[[{\"start_hour\":6,\"temp\":23.0,\"start_minute\":0},...],[...]]'" # ./broadlink-thermostat-cli.py --schedule='[[{"start_hour":6,"temp":22.5,"start_minute":0},{"start_hour":23,"temp":15.0,"start_minute":0},{"start_hour":23,"temp":15.0,"start_minute":0},{"start_hour":23,"temp":15.0,"start_minute":0},{"start_hour":23,"temp":15.0,"start_minute":0},{"start_hour":23,"temp":15.0,"start_minute":0}],[{"start_hour":6,"temp":22.5,"start_minute":0},{"start_hour":23,"temp":15.0,"start_minute":0}]]
